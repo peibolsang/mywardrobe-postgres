997ceb0fedf24145c74206643f3b7a78531f71db
Pablo Bermejo Garcia <pablo@Pablos-MacBook-Pro-16.local>
2025-08-02 22:46:25 +0200

    fix small layout issue

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -22,25 +22,27 @@ export function Navigation() {
   }, []);
 
   return (
-    <nav className="flex justify-between items-center my-8 mx-4">
-      <ul className="flex items-center space-x-4">
-        {links.map(({ href, label }) => (
-          <li key={href}>
-            <Link href={href} passHref>
-              <span
-                className={cn(
-                  "px-4 py-2 text-lg font-medium transition-colors relative",
-                  mounted && (pathname === href || (href === "/viewer" && pathname.startsWith("/garments")))
-                    ? "text-primary after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-primary"
-                    : "text-muted-foreground hover:text-primary"
-                )}
-              >
-                {label}
-              </span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="flex justify-between items-center my-8 w-full">
+      <div className="flex-grow flex justify-center">
+        <ul className="flex items-center space-x-4">
+          {links.map(({ href, label }) => (
+            <li key={href}>
+              <Link href={href} passHref>
+                <span
+                  className={cn(
+                    "px-4 py-2 text-lg font-medium transition-colors relative",
+                    mounted && (pathname === href || (href === "/viewer" && pathname.startsWith("/garments")))
+                      ? "text-primary after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[2px] after:bg-primary"
+                      : "text-muted-foreground hover:text-primary"
+                  )}
+                >
+                  {label}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div>
         {session ? (
           <button onClick={() => signOut()} className="px-4 py-2 text-lg font-medium transition-colors text-muted-foreground hover:text-primary">
