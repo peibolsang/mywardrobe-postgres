4d4509712ada310d015f2a6c103e246b9e5ef3fc
Pablo Bermejo Garcia <pablo@Pablos-MacBook-Pro-16.local>
2025-08-03 22:21:17 +0200

    fix navigation distribution

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -59,7 +59,7 @@ export function Navigation() {
 
    return (
     <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between px-8">
+      <div className="flex h-16 items-center justify-between px-8 w-full">
         {/* Logo/Brand */}
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
